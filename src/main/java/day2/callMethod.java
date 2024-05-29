6662ffecadf7a9ebc05ae52c77ce0592d68b7a88
joudar <joud_alrumayh@outlook.com>
2024-05-29 10:41:58 +0300

    fff

---
+++ src/main/java/day2/callMethod.java
@@ -0,0 +1,24 @@
+package day2;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class callMethod {
+    public static void main(String[] args) {
+//        Scanner sc =new Scanner(System.in);
+//        System.out.println("Enter weight and height: ");
+//        double w = sc.nextDouble();
+//        double h = sc.nextDouble();
+//
+//        double bmi =myMethods.calculateDMI(w,h);
+//        String status = myMethods.getStatus(bmi);
+//
+//        System.out.println("your Bmi"+ + bmi + ", " + status);
+
+        var statuses= myMethods.getStatus2(new double[] {23,20,35,15});
+        System.out.println(Arrays.toString(statuses));
+
+    }
+
+
+}
---
+++ src/main/java/day2/callMethod_EX1.java
@@ -0,0 +1,19 @@
+package day2;
+
+import java.util.Scanner;
+
+public class callMethod_EX1 {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter mark and fullmark: ");
+        double mark = sc.nextDouble();
+        double fullMark = sc.nextDouble();
+
+        double ptc = MyMethods_EX1.calculatePCT(mark, fullMark);
+        String status = MyMethods_EX1.getPCT(ptc);
+
+        System.out.println("your ptc"+ +ptc + ", " + status);
+
+
+    }
+}
